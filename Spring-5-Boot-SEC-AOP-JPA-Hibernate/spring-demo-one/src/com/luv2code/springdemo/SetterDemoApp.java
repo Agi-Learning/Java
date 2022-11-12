5e28f4aa08ab5a0d9a52fd8b7db64191d6f4071b
Agilan <agilandeena41@gmail.com>
2022-11-13 06:01:51 +0800

    Update SetterDemoApp.java

--- Spring-5-Boot-SEC-AOP-JPA-Hibernate/spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ Spring-5-Boot-SEC-AOP-JPA-Hibernate/spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -2,6 +2,30 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//7. Setter Injection
+//Spring Framework Inject dependencies by calling setter methods on your class 
+//
+//Development Process - Setter Injection
+//1. Create setter methods in your class for injections.
+//2. Configure the dependency injection in Spring config file.
+//
+//1. CricketCoach.java
+//public class CricketCoach implements Coach {
+//	private FortuneService fortuneService;
+//
+//	public CricketCoach() {}
+//
+//	public void setFortuneService(FortuneService fortuneService) {
+//		this.fortuneService = fortuneService;
+//	}
+//	then we have setter method for setFortuneService. This is the method that will be called by Spring when they inject the dependency using setter injection. 
+//}
+//
+//2. <propery name="fortuneService" ref="myFortuneService" />
+//
+//public void setFortuneService(...)
+//It'll take that property name and call the setter method on your class for that given propery. don't look the public method call setFortuneService, So Spring will actually call the setFortune Service and they'll pass in that value for that call. 
+//so in general, any property name you have, Spring will attempt to make a call to the setter method for that given property. 
 public class SetterDemoApp {
 
 	public static void main(String[] args) {
