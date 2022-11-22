caa1761cadca494b5c822d150ef26bce4a385827
Agilan <agilandeena41@gmail.com>
2022-11-22 17:01:23 +0800

    Spring 5 Boot AOP - 12. Hibernate Creating and Saving Java Objects
    Hibernate Creating and Saving Java Objects

---
+++ Spring-5-Boot-SEC-AOP-JPA-Hibernate/hibernate-tutorial/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,91 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+//31. Creating and Saving Java Objects
+//
+//2 Key Players
+//class 						Description
+//SessionFactory -->         		Reads the hibernate config file Create Session objects
+//							Heavy-weight object Only create once in your app
+//
+//Session        -->			Wraps a JDBC connection Main object used to save/      				                retrieve objects Short-lived object Retrieved from SessionFactory 
+//
+//Java Code Setup
+//public static void main(String[] args){
+//	SessionFactory factory = new Configuration()
+//							.configure("hibernate.cfg.xml")
+//							.addAnnotatedClass(Student.class)
+//							.buildSessionFactory();
+//
+//	Session session = factory.getCurrentSession();
+//
+//	try {
+//		// now use the session object to save/retrieve Java objects
+//	} finally {
+//		factory.close();
+//	}
+//}
+//This session object is a heavy-weight object, you really only create it once and then you reuse it x number of times throughout your application. 
+//Then i can use this factory to get a handle to a given session. So I say "factory.getCurrentSession();" It'll give me a session object, and then i can use it to perform a database operation. So, the normal pattern you'll have like, a try catch block or a try finally block, inside of the try block you actually use the session object to either save java objs or retrieve java objs. 
+//
+//Once we create the sessionFactory when we say ".configure", and i gave the actual name of the Hibernate configuration file, that's actually not required. You can simply say ".configure", open parent close parent
+//by default, Hibernate will look for a file name on your class path called "hibernate.cfg.xml", However, I like to be explicit during training just to show you, "Hey Hibernate's gonna look for a given file". However, the file name's not required as long as you use the default file name. But anyway, just wanted to give you the background on that
+//
+//Save a Java Object
+//
+//try {
+//	// create a student object
+//	Student tempStudent = new Student("paul", "Wall", "paul@luv2code.com");
+//
+//	// start transaction
+//	session.beginTransaction();
+//
+//	// save the student
+//	session.save(tempStudent);
+//
+//	// commit the transaction
+//	session.getTransaction().commit();
+//} finally {
+//	factory.close();
+//}
+//
+//Now, behind the scenes, right, Hibernate knows how to connect to our database based on our configuration file. Hibernate also knows how to map this student class or object to the actual database, based on those annotations we took care of in the prev video. So hibernate knows kinda where things should fall. So here I say "session.save", puts it in the database. And then I also do a commit on this transaction. So here as session.getTransaction().commit(). and that actually stores it in the database once i do a commit. There's also a rollback method if you'd like to do that, If you're not happy with this transaction.
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent = new Student("paul", "Wall", "paul@luv2code.com");
+
+			// start transaction
+			session.beginTransaction();
+
+			// save the student
+			System.out.println("Saving the student...");
+			session.save(tempStudent);
+
+			// commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
--- Spring-5-Boot-SEC-AOP-JPA-Hibernate/hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/Student.java
+++ Spring-5-Boot-SEC-AOP-JPA-Hibernate/hibernate-tutorial/src/com/luv2code/hibernate/demo/entity/Student.java
@@ -71,16 +71,16 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="student")
-public class Student {
+public class Student {	
 	
 	@Id
 	@Column(name="id")
 	private int id;
-	@Column(name="id")
+	@Column(name="first_name")
 	private String firstName;
-	@Column(name="id")
+	@Column(name="last_name")
 	private String lastName;
-	@Column(name="id")
+	@Column(name="email")
 	private String email;
 	
 	public Student() {
