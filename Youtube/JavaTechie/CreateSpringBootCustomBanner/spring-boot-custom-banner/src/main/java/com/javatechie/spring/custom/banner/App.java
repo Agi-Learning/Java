5a17b8512c3009b4b9676c786e3d204faa71647f
Agilan <agilandeena41@gmail.com>
2024-03-17 21:19:06 +0530

    Youtube Java Techie: Monitoring and Management Microservice using Spring Boot Admin Server
    Create Spring Boot  Custom Banner -Example

---
+++ Youtube/JavaTechie/CreateSpringBootCustomBanner/spring-boot-custom-banner/src/main/java/com/javatechie/spring/custom/banner/App.java
@@ -0,0 +1,11 @@
+package com.javatechie.spring.custom.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
